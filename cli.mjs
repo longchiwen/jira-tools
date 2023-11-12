8997305134e93aa84bc6021753bb51c298420b3a
Bostjan Jelen <bostjanj@gmail.com>
2023-11-12 17:35:59 +0100

    Added transition command

--- cli.mjs
+++ cli.mjs
@@ -1,31 +1,42 @@
 #!/usr/bin/env node
-import dotenv from 'dotenv-flow'
-dotenv.config()
+import dotenv from "dotenv-flow";
+dotenv.config();
 
-import fs from 'fs'
-import yargs from 'yargs'
+import fs from "fs";
+import yargs from "yargs";
 
 function readJsonFile(file) {
-    return JSON.parse(fs.readFileSync(file))
+  return JSON.parse(fs.readFileSync(file));
 }
 
 yargs
-    .command({
-        command:"bulk-close <file>",
-        desc:"Bulk close a list of issues",
-        handler: (argv)=> {
-            
-            (async () => {
-                const handler = await import('./bulk-close.mjs')
-                const config = readJsonFile(argv.file)
-                await handler.action.run(config)
-            })();
-
-        }
-    })
-    .demandCommand(1,"You need to specify a command. Try one of [bulk-close]")
-    .alias('v', 'version')
-    .version(readJsonFile('package.json').version)
-    .describe('v', 'show version information')
-    .help()
-    .argv
+  .command({
+    command: "bulk-close <file>",
+    desc: "Bulk close a list of issues",
+    handler: (argv) => {
+      (async () => {
+        const handler = await import("./bulk-close.mjs");
+        const config = readJsonFile(argv.file);
+        await handler.action.run(config);
+      })();
+    },
+  })
+  .command({
+    command: "transition <file>",
+    desc: "Run transition for a list of issues",
+    handler: (argv) => {
+      (async () => {
+        const handler = await import("./transition.mjs");
+        const config = readJsonFile(argv.file);
+        await handler.action.run(config);
+      })();
+    },
+  })
+  .demandCommand(
+    1,
+    "You need to specify a command. Try one of [bulk-close, transition]"
+  )
+  .alias("v", "version")
+  .version(readJsonFile("package.json").version)
+  .describe("v", "show version information")
+  .help().argv;
